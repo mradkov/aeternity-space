85917a81178b1eb8296f02274520329f1a2b5d2d
Milen Radkov <milenradkov@me.com>
2019-11-01 15:53:52 +0200

    setup axios
    setup axios for http requests

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import SmartTable from 'vuejs-smart-table'
 import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
 import App from './App.vue'
 import '@aeternity/aepp-components/dist/aepp.fonts.css'
 import '@aeternity/aepp-components/dist/aepp.components.css'
@@ -10,6 +11,8 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
 Vue.use(SmartTable)
 Vue.use(BootstrapVue)
+
+Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App),
