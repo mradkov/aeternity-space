383cc4e5133219bb848a1b5f0e7f8c935aba31c3
Milen Radkov <milenradkov@me.com>
2019-11-01 17:33:41 +0200

    use components in main

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import SmartTable from 'vuejs-smart-table'
 import BootstrapVue from 'bootstrap-vue'
 import axios from 'axios'
+import Components from '@aeternity/aepp-components-3';
 import App from './App.vue'
+import '@aeternity/aepp-components-3/dist/aepp.components.css';
 import '@aeternity/aepp-components/dist/aepp.fonts.css'
 import '@aeternity/aepp-components/dist/aepp.components.css'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -11,6 +13,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
 Vue.use(SmartTable)
 Vue.use(BootstrapVue)
+Vue.use(Components)
 
 Vue.prototype.$axios = axios;
 
