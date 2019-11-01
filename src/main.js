ad9dcb937a42be21c12388d9a3bf0dc90cf93524
Milen Radkov <milenradkov@me.com>
2019-11-01 14:44:25 +0200

    add dependencies
    added smart table
    added bootstrap framework

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+import SmartTable from 'vuejs-smart-table'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import '@aeternity/aepp-components/dist/aepp.fonts.css'
 import '@aeternity/aepp-components/dist/aepp.components.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(SmartTable)
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(App),
